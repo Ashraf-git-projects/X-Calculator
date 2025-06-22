a17b1262c718cfc337cf6b1c64f5197c9a33b47f
Ashraf <ashrafhussain2265@gmail.com>
2025-06-22 15:28:29 +0530

    First change

--- src/App.js
+++ src/App.js
@@ -1,69 +1,100 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-const [input , setInput] = useState('');
-const [output , setOutput] = useState('');
-const handleValue = (value)=>{
-  setInput((prev)=>prev+value);
-}
-const handleClear = ()=>{
-  setInput('');
-  setOutput('');
-}
-const handleEqual=()=>{
-  const result = evaluateResult(input);
-  setOutput(result);
-}
-const evaluateResult = (expr) =>{
-const tokens = expr.match(/(\d+|\+|\-|\*|\/)/g); // Split numbers and operators
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
 
-  if (!tokens || isNaN(tokens[0])) return 'Error';
+  const handleValue = (value) => {
+    setInput((prev) => prev + value);
+  };
 
-  let result = Number(tokens[0]);
+  const handleClear = () => {
+    setInput('');
+    setOutput('');
+  };
 
-  for (let i = 1; i < tokens.length; i += 2) {
-    const operator = tokens[i];
-    const nextNum = Number(tokens[i + 1]);
-
-    if (isNaN(nextNum)) return 'Error';
-
-    switch (operator) {
-      case '+': result += nextNum; break;
-      case '-': result -= nextNum; break;
-      case '*': result *= nextNum; break;
-      case '/': result /= nextNum; break;
-      default: return 'Error';
+  const handleEqual = () => {
+    if (!input || /[\+\-\*\/]$/.test(input)) {
+      setOutput('Error');
+      return;
     }
-  }
+    const result = evaluateResult(input);
+    setOutput(result);
+  };
 
-  return result;
-}
+  const evaluateResult = (expr) => {
+    // Replace 'x' with '*'
+    expr = expr.replace(/x/g, '*');
+
+    try {
+      // Tokenize the input: numbers and operators
+      const tokens = expr.match(/(\d+|\+|\-|\*|\/)/g);
+      if (!tokens) return 'Error';
+
+      // First handle * and /
+      let newTokens = [];
+      let i = 0;
+      while (i < tokens.length) {
+        if (tokens[i] === '*' || tokens[i] === '/') {
+          const prev = Number(newTokens.pop());
+          const next = Number(tokens[i + 1]);
+          if (tokens[i] === '/') {
+            newTokens.push(prev / next);
+          } else {
+            newTokens.push(prev * next);
+          }
+          i += 2;
+        } else {
+          newTokens.push(tokens[i]);
+          i++;
+        }
+      }
+
+      // Then handle + and -
+      let result = Number(newTokens[0]);
+      for (let j = 1; j < newTokens.length; j += 2) {
+        const operator = newTokens[j];
+        const nextNum = Number(newTokens[j + 1]);
+        if (operator === '+') result += nextNum;
+        else if (operator === '-') result -= nextNum;
+        else return 'Error';
+      }
+
+      if (isNaN(result)) return 'NaN';
+      if (!isFinite(result)) return 'Infinity';
+      return result;
+    } catch (err) {
+      return 'Error';
+    }
+  };
+
   return (
     <div className="App">
-       <h1>React Calculator</h1>
-       <div className='calc'>
-        <input className='input' value={input} readOnly></input>
-        <p className='output'>{output}</p>
+      <h1>React Calculator</h1>
+      <div className='calc'>
+        <input className='input' value={input} readOnly />
+        <div className='output'>{output}</div>
+
         <div className='cal_btns'>
-        <button className='btn' onClick={()=>handleValue('1')}>1</button>
-        <button className='btn' onClick={()=>handleValue('2')}>2</button>
-        <button className='btn' onClick={()=>handleValue('3')}>3</button>
-        <button className='btn' onClick={()=>handleValue('4')}>4</button>
-        <button className='btn' onClick={()=>handleValue('5')}>5</button>
-        <button className='btn' onClick={()=>handleValue('6')}>6</button>
-        <button className='btn' onClick={()=>handleValue('7')}>7</button>
-        <button className='btn' onClick={()=>handleValue('8')}>8</button>
-        <button className='btn' onClick={()=>handleValue('9')}>9</button>
-        <button className='btn' onClick={()=>handleValue('0')}>0</button>
-        <button className='btn' onClick={()=>handleValue('+')}>+</button>
-        <button className='btn' onClick={()=>handleValue('-')}>-</button>
-        <button className='btn' onClick={()=>handleValue('x')}>x</button>
-        <button className='btn' onClick={()=>handleValue('/')}>/</button>
-        <button className='btn' onClick={()=>handleClear()}>C</button>
-        <button className='btn' onClick={()=>handleEqual()}>=</button>
+          <button className='btn' onClick={() => handleValue('1')}>1</button>
+          <button className='btn' onClick={() => handleValue('2')}>2</button>
+          <button className='btn' onClick={() => handleValue('3')}>3</button>
+          <button className='btn' onClick={() => handleValue('4')}>4</button>
+          <button className='btn' onClick={() => handleValue('5')}>5</button>
+          <button className='btn' onClick={() => handleValue('6')}>6</button>
+          <button className='btn' onClick={() => handleValue('7')}>7</button>
+          <button className='btn' onClick={() => handleValue('8')}>8</button>
+          <button className='btn' onClick={() => handleValue('9')}>9</button>
+          <button className='btn' onClick={() => handleValue('0')}>0</button>
+          <button className='btn' onClick={() => handleValue('+')}>+</button>
+          <button className='btn' onClick={() => handleValue('-')}>-</button>
+          <button className='btn' onClick={() => handleValue('*')}>*</button>
+          <button className='btn' onClick={() => handleValue('/')}>/</button>
+          <button className='btn' onClick={handleClear}>C</button>
+          <button className='btn' onClick={handleEqual}>=</button>
         </div>
-       </div>
+      </div>
     </div>
   );
 }
